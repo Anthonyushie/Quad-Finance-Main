a950fadff0c2ded62b7026a098e51e76260d6a12
Wutche <wuthchechikaome75@gmail.com>
2024-08-16 15:34:32 -0700

    push

---
+++ src/app/providers.tsx
@@ -0,0 +1,7 @@
+/** @format */
+
+import { NextUIProvider } from "@nextui-org/system";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+	return <NextUIProvider>{children}</NextUIProvider>;
+}
